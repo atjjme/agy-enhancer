fb3181beb8b05115bdbe433c36026c1777ff153e
atjjme <4524111+atjjme@users.noreply.github.com>
2026-09-06 17:45:30 +0800

    构建：更新主干 main 用户脚本产物

--- agy-enhancer.user.js
+++ agy-enhancer.user.js
@@ -11,8 +11,6 @@
 // @run-at       document-idle
 // ==/UserScript==
 
-window.__AGY_BRANCH_TAG__ = " (branch)";
-window.__AGY_BRANCH_NAME__ = "starlit_pulsar_rises_17h27";
 /**
  * Antigravity 增强器 (agy-enhancer enhancer)
  * 
