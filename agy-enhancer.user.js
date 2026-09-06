c313d3aa5ac5229b3d2e44990cff671ff20c921f
atjjme <4524111+atjjme@users.noreply.github.com>
2026-09-06 09:47:36 +0800

    chore: main 主分支构建发布版本油猴脚本

--- agy-enhancer.user.js
+++ agy-enhancer.user.js
@@ -11,8 +11,6 @@
 // @run-at       document-idle
 // ==/UserScript==
 
-window.__AGY_BRANCH_TAG__ = " (branch)";
-window.__AGY_BRANCH_NAME__ = "move_copy_to_submenu";
 /**
  * Antigravity 增强器 (agy-enhancer enhancer)
  * 
