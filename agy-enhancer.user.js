0139dee381c71910e752d16ab95f52c2d91ab31c
atjjme <4524111+atjjme@users.noreply.github.com>
2026-09-06 17:11:31 +0800

    build: 重新构建 main 分支油猴脚本

--- agy-enhancer.user.js
+++ agy-enhancer.user.js
@@ -11,8 +11,6 @@
 // @run-at       document-idle
 // ==/UserScript==
 
-window.__AGY_BRANCH_TAG__ = " (branch)";
-window.__AGY_BRANCH_NAME__ = "golden_comet_arcs_10h20";
 /**
  * Antigravity 增强器 (agy-enhancer enhancer)
  * 
