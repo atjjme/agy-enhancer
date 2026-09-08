b685e0c405bee4a45974caf32ae9a9c1214b741d
atjjme <4524111+atjjme@users.noreply.github.com>
2026-09-08 20:05:06 +0800

    chore(build): 构建 main 主分支最新油猴增强脚本

--- scripts/agy-enhancer.user.js
+++ scripts/agy-enhancer.user.js
@@ -11,8 +11,6 @@
 // @run-at       document-idle
 // ==/UserScript==
 
-window.__AGY_BRANCH_TAG__ = " (branch)";
-window.__AGY_BRANCH_NAME__ = "spectral_cosmos_floats_19h42";
 /**
  * Antigravity 增强器 (agy-enhancer enhancer)
  * 
