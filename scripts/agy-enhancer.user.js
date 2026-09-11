3ae1e5722218850737566af9b3366bccc1c876a5
atjjme <4524111+atjjme@users.noreply.github.com>
2026-09-11 14:41:03 +0800

    chore(build): 构建 main 主分支最新油猴增强脚本

--- scripts/agy-enhancer.user.js
+++ scripts/agy-enhancer.user.js
@@ -11,8 +11,6 @@
 // @run-at       document-idle
 // ==/UserScript==
 
-window.__AGY_BRANCH_TAG__ = " (branch)";
-window.__AGY_BRANCH_NAME__ = "implement_split_screen";
 /**
  * Antigravity 增强器 (agy-enhancer enhancer)
  * 
